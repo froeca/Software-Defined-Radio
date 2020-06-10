77451b11e7927f1d2289b7cbc3b4823ccc05bce6
froeca <Caleb.Froelich@wallawalla.edu>
2020-06-10 15:38:02 -0400

    Added presentation slides and modified quisk test program.

--- Quisk/Arduino/SDR_Test1/SDR_Test1.ino
+++ Quisk/Arduino/SDR_Test1/SDR_Test1.ino
@@ -9,7 +9,7 @@ void setup()
 {
   bool i2c_found;
   i2c_found = si5351.init(SI5351_CRYSTAL_LOAD_10PF, 0, 0);
-  si5351.set_freq(4*1400000000ULL, SI5351_CLK0);
+  si5351.set_freq(4*5000000ULL, SI5351_CLK0);
   Serial.begin(115200);
 }
 
